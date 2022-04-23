9ab0cdf83e3713e5e9ef22050cd06a8490a49208
rakibul158 <rakib3665@gmail.com>
2022-04-24 01:24:55 +0600

    index file moddify

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Register Form</title>
-		<link rel="stylesheet" href="css/style.css" />
-	</head>
-	
-	<body>
-        <div class="container">
-            <div class="wrapper">
-                <div class="register-form">
-                    <div class="register-start">
-                        <h2>Register Form</h2>
-                    </div>
-                    <div class="register-end">
-                        <div class="form">
-                            <div class="form-field">
-                                <div class="form-level">Name</div>
-                                <input type="text" name="name" class="form-input" placeholder="Name">
-                            </div>
-                            <div class="form-field">
-                                <div class="form-level">Email</div>
-                                <input type="text" name="email" class="form-input" placeholder="E-mail">
-                            </div>
-                            <div class="form-field">
-                                <div class="form-level">Phone</div>
-                                <input type="text" name="phone" class="form-input" placeholder="Phone">
-                            </div>
-                            <div class="form-field">
-                                <button class="btn-submit">Submit</button>
-                            </div>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <div class="register-form">
+                <div class="register-start">
+                    <h2>Register Form</h2>
+                </div>
+                <div class="register-end">
+                    <div class="form">
+                        <div class="form-field">
+                            <div class="form-level">Name</div>
+                            <input type="text" name="name" class="form-input" placeholder="Name" />
+                        </div>
+                        <div class="form-field">
+                            <div class="form-level">Email</div>
+                            <input type="text" name="email" class="form-input" placeholder="E-mail" />
+                        </div>
+                        <div class="form-field">
+                            <div class="form-level">Phone</div>
+                            <input type="text" name="phone" class="form-input" placeholder="Phone" />
+                        </div>
+                        <div class="form-field">
+                            <button class="btn-submit">Submit</button>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-	</body>
+    </div>
+</body>
 </html>
