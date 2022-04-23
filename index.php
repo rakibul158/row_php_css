100daf33085a206c6046a1e127f48e43153ccba4
rakibul158 <rakib3665@gmail.com>
2022-04-24 01:19:34 +0600

    Initail setup row_php_css

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Register Form</title>
+		<link rel="stylesheet" href="css/style.css" />
+	</head>
+	
+	<body>
+        <div class="container">
+            <div class="wrapper">
+                <div class="register-form">
+                    <div class="register-start">
+                        <h2>Register Form</h2>
+                    </div>
+                    <div class="register-end">
+                        <div class="form">
+                            <div class="form-field">
+                                <div class="form-level">Name</div>
+                                <input type="text" name="name" class="form-input" placeholder="Name">
+                            </div>
+                            <div class="form-field">
+                                <div class="form-level">Email</div>
+                                <input type="text" name="email" class="form-input" placeholder="E-mail">
+                            </div>
+                            <div class="form-field">
+                                <div class="form-level">Phone</div>
+                                <input type="text" name="phone" class="form-input" placeholder="Phone">
+                            </div>
+                            <div class="form-field">
+                                <button class="btn-submit">Submit</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+	</body>
+</html>
